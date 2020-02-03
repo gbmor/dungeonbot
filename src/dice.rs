e0ee08d05bfa17ddb169532fff05ae32a6ba99b5
Ben Morrison <ben@gbmor.dev>
2020-02-03 02:15:12 -0500

    string manipulation is fun

--- src/dice.rs
+++ src/dice.rs
@@ -1,11 +1,10 @@
 use rand::prelude::*;
 
 fn roll(msg: &str) -> String {
-    unimplemented!();
     let mut rng = rand::thread_rng();
 
     let chars = msg.chars().map(|c| c.to_string()).collect::<Vec<String>>();
-    let dice_type = String::from("6"); // format!("{}", chars[2..]);
+    let dice_type = chars[2..].to_vec().into_iter().collect::<String>();
 
     let upper_bound: u32 = if let Ok(val) = dice_type.parse::<u32>() {
         val + 1
